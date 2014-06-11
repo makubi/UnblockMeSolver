5c64c38c58802234cc9fe4c8d40274b323cebcc3
Florian Witteler <f.witteler@douglas.de>
2014-06-11 12:00:41 +0200

    non akka algorithm version (the inefficient one) removed

--- src/main/scala/solver/Location.scala
+++
@@ -1,6 +0,0 @@
-package solver
-
-/**
- * Created by flwi on 21.05.14.
- */
-case class Location(x: Int, y: Int)
--- src/main/scala/solver/Orientation.scala
+++
@@ -1,6 +0,0 @@
-package solver
-
-object Orientation extends Enumeration {
-  type Orientation = Value
-  val Horizontal, Vertical = Value
-}
